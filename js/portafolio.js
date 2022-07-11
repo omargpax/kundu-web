6e8bda02001531405795653f53852ed604b22e0e
Omar guerrero <omarguerrero@upeu.edu.pe>
2022-07-11 13:11:09 -0500

    portafolio

---
+++ js/portafolio.js
@@ -0,0 +1,15 @@
+function inicio() {
+    window.location.href="/index.html";
+}
+
+var card1 = document.getElementById("lock1");
+card1.style.backgroundImage="url('https://images.unsplash.com/photo-1655374631048-1416d81cd20a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=798&q=80')";
+
+var card2 = document.getElementById("lock2");
+card2.style.backgroundImage="url('https://i.ibb.co/Gp4Y7W3/botw.jpg')";
+
+var card3= document.getElementById("lock3");
+card3.style.backgroundImage="url('https://i.ibb.co/2YX14s6/Whats-App-Image-2022-07-11-at-4-41-47-AM.jpg')";
+
+var card4 = document.getElementById("lock4");
+card4.style.backgroundImage="url('https://i.ibb.co/hmP3Qbd/Kundu-Room.jpg')";
