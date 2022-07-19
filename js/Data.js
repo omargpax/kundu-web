bc22239472470850210331bf40c615a5167c8f8b
Omar guerrero <omarguerrero@upeu.edu.pe>
2022-07-18 22:03:24 -0500

    add shoping cart

---
+++ js/Data.js
@@ -0,0 +1,17 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Polera kundu",
+      price: 65,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "media/poler_kundu.jpg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Sudadera",
+      price: 75,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "media/sudadera_kundu.jpg",
+    },
+  ];
+  
